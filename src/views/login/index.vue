0d687b95f68929a1d076ef29f38650839bfac2c7
Hyper18 <windhyperspace@gmail.com>
2024-04-16 17:16:40 +0800

    用户登录与用户资质申请

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,89 +1,136 @@
 <template>
-  <div class="login-container">
-    <el-form
-      ref="loginForm"
-      :model="loginForm"
-      :rules="loginRules"
-      class="login-form"
-      auto-complete="on"
-      label-position="left"
+  <div class="outer-container">
+    <div class="login-container">
+      <el-form
+        ref="loginForm"
+        :model="loginForm"
+        :rules="loginRules"
+        class="login-form"
+        auto-complete="on"
+        label-position="left"
+      >
+        <div class="title-container">
+          <h3 class="title">细粒度多层级身份认证软件</h3>
+        </div>
+
+        <el-form-item prop="username">
+          <span class="svg-container">
+            <svg-icon icon-class="user" />
+          </span>
+          <el-input
+            ref="username"
+            v-model="loginForm.username"
+            placeholder="请输入您的用户名"
+            name="username"
+            type="text"
+            tabindex="1"
+            auto-complete="on"
+          />
+        </el-form-item>
+
+        <el-form-item prop="password">
+          <span class="svg-container">
+            <svg-icon icon-class="password" />
+          </span>
+          <el-input
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="请输入对应的密码"
+            name="password"
+            tabindex="2"
+            auto-complete="on"
+            @keyup.enter.native="handleLogin"
+          />
+          <span class="show-pwd" @click="showPwd">
+            <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+          </span>
+        </el-form-item>
+
+        <div class="login-form__footer">
+          <el-link type="primary" class="apply-link" @click="handleApply">点击此处申请用户资质</el-link>
+          <el-button
+            :loading="loading"
+            type="primary"
+            style="width:100%;margin-bottom:30px;"
+            @click.native.prevent="handleLogin"
+          >登 录</el-button>
+        </div>
+      </el-form>
+
+      <div class="info" style="bottom: 40px;">版本号: 0.1.3</div>
+      <div class="info">开发团队: SEU 319</div>
+    </div>
+
+    <el-drawer
+      class="info-drawer"
+      title="正在为您申请用户资质..."
+      :before-close="handleDrawerClose"
+      :visible.sync="drawerVisible"
+      direction="rtl"
+      custom-class="info-drawer"
     >
-      <div class="title-container">
-        <h3 class="title">细粒度多层级身份认证软件</h3>
-      </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon icon-class="user" />
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon icon-class="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-button
-        :loading="loading"
-        type="primary"
-        style="width:100%;margin-bottom:30px;"
-        @click.native.prevent="handleLogin"
-      >登 录</el-button>
-    </el-form>
-
-    <div class="info" style="bottom: 40px;">版本号: 0.1.0</div>
-    <div class="info">开发团队: SEU 319</div>
+      <div class="info-drawer__content">
+        <el-form :model="editedUser" label-position="left">
+          <el-form-item label="用户名" :label-width="formLabelWidth" required>
+            <el-input v-model="editedUser.user_name" placeholder="请输入您想使用的用户名..." />
+          </el-form-item>
+          <el-form-item label="用户密码" :label-width="formLabelWidth" required>
+            <el-input v-model="editedUser.user_password" placeholder="请输入您的密码..." show-password />
+          </el-form-item>
+          <el-form-item label="重复密码" :label-width="formLabelWidth" required>
+            <el-input v-model="editedUser.user_password_repeat" placeholder="请再次输入您的密码..." show-password />
+          </el-form-item>
+          <el-form-item label="所属组织" :label-width="formLabelWidth" required>
+            <el-input v-model="editedUser.user_role" placeholder="请再次输入您所在的公司或组织..." />
+          </el-form-item>
+          <el-form-item label="用户邮箱" :label-width="formLabelWidth">
+            <el-input v-model="editedUser.user_email" placeholder="请输入您想使用的邮箱..." />
+          </el-form-item>
+          <el-form-item label="用户手机号" :label-width="formLabelWidth">
+            <el-input v-model="editedUser.user_phone" placeholder="请输入您的手机号..." />
+          </el-form-item>
+        </el-form>
+        <div class="info-drawer__footer">
+          <el-button @click="handleDrawerClose">取 消</el-button>
+          <el-button
+            type="primary"
+            :loading="drawerLoading"
+            @click="handleDrawerSubmit"
+          >
+            {{ drawerLoading ? '提交中 ...' : '提 交' }}
+          </el-button>
+        </div>
+      </div></el-drawer>
   </div>
 </template>
 
 <script>
 import { validUsername } from '@/utils/validate'
+import { userApply } from '@/api/user'
 
 export default {
   name: 'Login',
   data() {
     const validateUsername = (rule, value, callback) => {
       if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
+        callback(new Error('（Mock）请输入用户名: admin / editor'))
       } else {
         callback()
       }
     }
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
+        callback(new Error('（Mock）任填一个大于6位的密码'))
       } else {
         callback()
       }
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: 'admin123'
+        username: '',
+        password: ''
       },
       loginRules: {
         username: [
@@ -93,6 +140,11 @@ export default {
           { required: true, trigger: 'blur', validator: validatePassword }
         ]
       },
+      formLabelWidth: '100px',
+      drawerLoading: false,
+      drawerVisible: false,
+      drawerTitle: '',
+      editedUser: {},
       loading: false,
       passwordType: 'password',
       redirect: undefined
@@ -131,10 +183,73 @@ export default {
               this.loading = false
             })
         } else {
-          console.log('error submit!!')
+          // console.log('提交有误！')
           return false
         }
       })
+    },
+    handleApply() {
+      this.editedUser = {}
+      this.drawerVisible = true
+    },
+    drawerReset() {
+      this.drawerLoading = false
+      this.drawerTitle = ''
+      clearTimeout(this.drawerTimer)
+      this.drawerVisible = false
+    },
+    handleDrawerClose(done) {
+      this.$confirm('', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        message:
+        `<div style="white-space: pre-wrap">确认关闭表单吗？<br><span style="color: red;">注意：</span>此操作将放弃当前所有更改！</div>`,
+        dangerouslyUseHTMLString: true
+      })
+        .then(_ => {
+          this.drawerReset()
+          done()
+        })
+        .catch(_ => {})
+    },
+    handleDrawerSubmit() {
+      if (this.drawerLoading) {
+        return
+      }
+      this.$confirm('确定要提交表单吗？', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.drawerLoading = true
+          this.drawerTimer = setTimeout(() => {
+            // eslint-disable-next-line no-unused-vars
+            const { user_password_repeat, ...updateForm } = this.editedUser
+            if (user_password_repeat !== this.editedUser.user_password) {
+              return
+            }
+            userApply(this.editedUser.user_id, updateForm)
+              .then(response => {
+                if (response.data.status === 'success') {
+                  this.getData()
+                  this.$message.success('用户资质申请成功！')
+                } else {
+                  this.$message.error('用户资质申请失败！')
+                }
+
+                setTimeout(() => {
+                  this.drawerReset()
+                }, 200)
+              })
+              .catch(() => {
+                this.$message.error('用户资质申请失败，请稍后再试！')
+                this.drawerReset()
+              })
+          }, 1000)
+        })
+        .catch(() => {})
     }
   }
 }
@@ -198,6 +313,36 @@ $cursor: #fff;
 $bg: #2d3a4b;
 $dark_gray: #889aa4;
 $light_gray: #eee;
+
+.outer-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.apply-link {
+  font-size: 14px;
+  color: $light_gray;
+  margin-bottom: 10px;
+  display: inline-block;
+}
+
+.info-drawer__content {
+  padding: 20px;
+  height: 90%;
+  overflow-y: auto;
+}
+
+.info-drawer__footer {
+  padding: 20px;
+  display: flex;
+  justify-content: space-between, flex-end;
+}
+
+.info-drawer__footer .el-button:first-child {
+  margin-left: auto;
+  margin-right: 2%;
+}
 
 .login-container {
   min-height: 100%;
